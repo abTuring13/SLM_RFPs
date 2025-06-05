ef24cfd23feece72f58c954b98642834f1a6532a
abTuring13 <antoniborys131@gmail.com>
2025-06-05 15:54:18 +0200

    Add virtual environment setup and numpy example

--- greetings.py
+++ greetings.py
@@ -1,9 +1,12 @@
+import numpy
         
 def main():# This is the correct way to make code run only when the file is executed directly
     if __name__ == "__main__":
         print("Main code executed")
 
-
+def npexample():
+    ar1 = numpy.array([1,2,3,4,5])
+    print(numpy.mean(ar1))
 
 #this exectued only if run in greetings.py
 main()
