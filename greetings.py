238531ca21669a13b1a6aa16e64d8d8eb190afe2
abTuring13 <antoniborys131@gmail.com>
2025-06-05 14:07:55 +0200

    This is first module to demonstrate how __name__ works

---
+++ greetings.py
@@ -0,0 +1,9 @@
+        
+def main():# This is the correct way to make code run only when the file is executed directly
+    if __name__ == "__main__":
+        print("Main code executed")
+
+
+
+#this exectued only if run in greetings.py
+main()
